0e3673bfb0eb9145be50265b4beb718ad368e25e
Thom Le <thomle0418@gmail.com>
2022-05-31 14:56:42 -0600

    created resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    },
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID
+        user: User
+    }
+    input bookInput {
+        bookId: String!
+        authors: [String]
+        description: String!
+        image: String!
+        link: String!
+    }
+    type Query {
+        me: Users
+    
+    }
+    type Mutation {
+        login(email: String!, password: String!); Auth
+        addUser(username: String!, email: String! password: String): Auth
+        saveBook(input: bookInput!): User
+        removeBook(bookId: Sting!): User
+    }
+`;
+
+module.exports = typeDefs;
